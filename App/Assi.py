35a77928c5a680ba7b68966e6b91efdd1fc204df
timneuenschwander <63901857+timneuenschwander@users.noreply.github.com>
2023-05-13 14:42:14 +0200

    ml connection

--- App/Assi.py
+++ App/Assi.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 import webbrowser
 import requests
-import json
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -43,14 +44,40 @@ def getdata():
     print(track_response)
 
     # Extract the track data from the response
-    acousticness = track_data_json['acousticness']
     danceability = track_data_json['danceability']
-    duration_ms = track_data_json['duration_ms']
     energy = track_data_json['energy']
+    key = track_data_json['key']
+    loudness = track_data_json['loudness']
+    mode = track_data_json['mode']
+    speechiness = track_data_json['speechiness']
+    acousticness = track_data_json['acousticness']
     instrumentalness = track_data_json['instrumentalness']
+    liveness = track_data_json['liveness']
+    valence = track_data_json['valence']
+    tempo = track_data_json['tempo']
+    duration_ms = track_data_json['duration_ms']
+    time_signature = track_data_json['time_signature']
+    chorus_hit = 30.215
+    sections = 10
+
+    X_test = [[danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, time_signature, chorus_hit, sections]]
+
+    #Create a df with the feature names and X Test as first row
+    feature_names = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness','valence', 'tempo', 'duration_ms', 'time_signature', 'chorus_hit', 'sections']
+    X_test_df = pd.DataFrame(X_test, columns=feature_names)
+    
+    #load pickle and test if it works
+    xgb_model_loaded = pickle.load(open('xgb_model.pkl', 'rb'))
+    #print(xgb_model_loaded.predict(X_test_df))
+    # Make predictions using the loaded model
+    prediction = xgb_model_loaded.predict(X_test_df)
+    if prediction > 0.5:
+        prediction_label = 'Hit'
+    else:
+        prediction_label = 'Flop'
 
     # Process the track data as needed
-    return render_template('index2.html', track_data=track_data_json, acousticness=acousticness, danceability=danceability, duration_ms=duration_ms, energy=energy, instrumentalness=instrumentalness)
+    return render_template('index2.html', track_data=track_data_json, danceability=danceability, energy=energy, key=key, loudness=loudness, mode=mode, speechiness=speechiness, acousticness=acousticness, instrumentalness=instrumentalness, liveness=liveness, valence=valence, tempo=tempo, duration_ms=duration_ms, time_signature=time_signature, chorus_hit=chorus_hit, sections=sections, prediction=prediction_label)
     
 if __name__ == '__main__':
     app.run(debug=True)
