01db205a55ac8b243d4c0910f6776a8aeaa7db47
mrtz-wssl <126496491+mrtz-wssl@users.noreply.github.com>
2023-05-23 16:36:28 +0200

    update + data check

--- App/Assi.py
+++ App/Assi.py
@@ -117,7 +117,7 @@ def getdata():
         'q': f'artist:{artist_name} track:{track_name}',
         'type': 'track',
         'limit': 1
-    }
+    }  
  
     headers = {'Authorization': f'Bearer {access_token}'}
 
@@ -208,6 +208,8 @@ def getdata():
 
 
 #END SM-TARGET PREDICTION
+
+
     #### Create a graph: ####
     graph_file_path = os.path.join('App/static/images', 'feature_graph.png')
 
